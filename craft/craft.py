c81873162fcb8803496542df433d49d62d7530c1
the7who <35671829+the7who@users.noreply.github.com>
2018-06-01 15:12:34 -0700

    Update craft.py

--- craft/craft.py
+++ craft/craft.py
@@ -122,6 +122,116 @@ def craft():
    # Print out stats
    print('Best average flat damage:', best_avg_flat_dmg)
    return 'Completed crafting'
+
+def craft_dex_boots():
+   FRW_THRESH = 30
+   PDEX_THRESH = 8
+   DEX_THRESH = 55
+   LIFE_THRESH = 150
+   print('\n\nStarting crafting loop...\n\n')
+   # Loop until we're done, each iteration is one character
+   done = False
+   while (not done):
+      # Track best average damage per character
+      best_avg_flat_dmg = 0
+      corr_enh_dmg = 0
+      # Restore character
+      character = backrest.restore_recent(BACKUP_ALL_OVERWRITTEN_FILES)
+      if (VERBOSE): print('Restored', character)
+      # Create singleplayer game
+      mouse.move(*median.get_coords('singleplayer'))
+      mouse.click()
+      keyboard.send('enter')
+      if (VERBOSE): print('Entering new game...')
+      median.wait_enter_game()
+      if (VERBOSE): print('Entered game')
+      # Open inventory
+      time.sleep(1)
+      keyboard.send('i')
+      time.sleep(.5)
+      # Loop through each shrine
+      shrine_size = 1, 2 # width, height
+      for i, coords in enumerate(median.inv_iter(*shrine_size)):
+         if done: break
+         # Skip first two "shrines", these are where the cube is
+         if (i < 2): continue
+         # Place shrine into cube
+         #   1) Grab shrine
+         mouse.move(*coords)
+         mouse.click()
+         sleep_ms(50)
+         #   2) Place in cube
+         mouse.move(*median.get_coords('cube'))
+         sleep_ms(50)
+         mouse.click()
+         sleep_ms(50)
+         #   3) Open cube only if first shrine
+         if (i == 2):
+            mouse.right_click()
+            sleep_ms(50)
+         # Loop through a shrine's 10 charges
+         for charge in range(10):
+            # Ensure that if the window loses focus, loop exits
+            if (not util.check_active_window()):
+               print('Game window lost focus, exiting crafting loop.')
+               done = True
+               break
+            # Transmute
+            mouse.move(*median.get_coords('transmute'))
+            sleep_ms(10)
+            mouse.click()
+            sleep_ms(10)
+            # Mouseover item
+            mouse.move(*median.get_coords('cubetopleft'))
+            sleep_ms(100)
+            # Get text
+            item_stats = median.get_item_text()
+            dex = item_parser.get_attr(item_stats, "Dexterity")
+            life = item_parser.get_life(item_stats)
+            frw = item_parser.get_frw(item_stats)
+            pdex = item_parser.get_pattr(item_stats, "Dexterity")
+            #if (avg_flat_dmg > best_avg_flat_dmg):
+            #   best_avg_flat_dmg = avg_flat_dmg
+            #   corr_enh_dmg = enh_dmg
+            # log_string = '=========================\n\n' + item_stats + '\n'
+            # util.log(log_string, 'item_log.txt')
+            if ( (dex >= DEX_THRESH) and (pdex >= PDEX_THRESH) ):
+               print("Dex:{0}  Life:{1}   frw:{2}".format(dex, life, frw))
+            sleep_ms(100)
+            # Check if we are done
+            if ( (dex >= DEX_THRESH) and (life >= LIFE_THRESH) and (pdex >= PDEX_THRESH) and (frw >= FRW_THRESH) ):
+               done = True
+               break
+         # End of shrine charges loop
+         # For testing, break early
+         # if (i == 3): break
+      # End of shrines loop
+      # Print out best average damage for this character
+      print('Finished this character')
+      #print('   best average flat damage: ', best_avg_flat_dmg, sep='')
+      #print('   corresponding enh damage: ', corr_enh_dmg, '%', sep='')
+      #print()
+      # Exit game
+      if (not done):
+         keyboard.send('esc')
+         sleep_ms(150)
+         keyboard.send('esc')
+         mouse.move(*median.get_coords('exit'))
+         if (VERBOSE): print('Exiting game...')
+         mouse.click()
+         sleep_ms(100)
+         median.wait_exit_game()
+         if (VERBOSE): print('Exited game...')
+         sleep_ms(100)
+      # Ensure that if the window loses focus, loop exits
+      if (not util.check_active_window()):
+         print('Game window lost focus, exiting crafting loop.')
+         break
+   # End of while loop
+   ##
+   # Print out stats
+   print('Best average flat damage:', best_avg_flat_dmg)
+   return 'Completed crafting'
       
    
 def main():
